d20b7cc14a6384cb221e80332229275179fb4ff6
Leonardo Rissi <leonardomelorissi@gmail.com>
2021-09-23 19:06:41 -0300

    Removendo a repeticao dos componentes cards

--- pastecco-pedidos-app/src/components/UI/CardWrapper/CardWrapper.jsx
+++ pastecco-pedidos-app/src/components/UI/CardWrapper/CardWrapper.jsx
@@ -1,48 +1,25 @@
 import React from 'react';
 import Card from 'components/UI/Card/Card'
-import { CardsWrapper } from 'components/SharedStyles'
-import {
-    CARD_IMAGES_PATH,
-    CARD_TITLES,
-    CARD_DESCRIPTIONS,
-    CARD_PRICES,
-} from './CardConstants'
+import { CardsWrapper } from './CardWrapperStyles'
+import { CARDS } from './CardConstants'
 
 const CardWrapper = () => {
+    const cardsArray = []
+    CARDS.forEach((card) => {
+        cardsArray.push(
+            <Card
+                imgPath={card.CARD_IMAGE_PATH}
+                title={card.CARD_TITLE}
+                description={card.CARD_DESCRIPTION}
+                price={card.CARD_PRICE}
+            />
+        );
+    })
     return (
         <CardsWrapper>
-            <Card
-                imgPath={CARD_IMAGES_PATH.CHOCOLATE_COM_MORAGO}
-                title={CARD_TITLES.CHOCOLATE_COM_MORAGO}
-                description={CARD_DESCRIPTIONS.CHOCOLATE_COM_MORAGO}
-                price={CARD_PRICES.CHOCOLATE_COM_MORAGO}
-            />
-            <Card
-                imgPath={CARD_IMAGES_PATH.CAMARAO_CREMOSO}
-                title={CARD_TITLES.CAMARAO_CREMOSO}
-                description={CARD_DESCRIPTIONS.CHOCOLATE_COM_MORAGO}
-                price={CARD_PRICES.CHOCOLATE_COM_MORAGO}
-            />
-            <Card
-                imgPath={CARD_IMAGES_PATH.FRANGO_MINEIRO}
-                title={CARD_TITLES.FRANGO_MINEIRO}
-                description={CARD_DESCRIPTIONS.CHOCOLATE_COM_MORAGO}
-                price={CARD_PRICES.CHOCOLATE_COM_MORAGO}
-            />
-            <Card
-                imgPath={CARD_IMAGES_PATH.CARNE}
-                title={CARD_TITLES.CARNE}
-                description={CARD_DESCRIPTIONS.CHOCOLATE_COM_MORAGO}
-                price={CARD_PRICES.CHOCOLATE_COM_MORAGO}
-            />
-            <Card
-                imgPath={CARD_IMAGES_PATH.ROMEU_JULIETA}
-                title={CARD_TITLES.ROMEU_JULIETA}
-                description={CARD_DESCRIPTIONS.CHOCOLATE_COM_MORAGO}
-                price={CARD_PRICES.CHOCOLATE_COM_MORAGO}
-            />
+            {cardsArray}
         </CardsWrapper>
-    );
+    )
 }
 
 export default CardWrapper;
---
+++ pastecco-pedidos-app/src/components/UI/CardWrapper/CardWrapperStyles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const CardsWrapper = styled.section`
+    max-width: 800px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 10px;
+    padding: 14px;
+`;
