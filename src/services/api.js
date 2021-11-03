6aca5b1ea9e01a7291b2ec712f5ac15b16b0055f
Leonardo Rissi <leonardomelorissi@gmail.com>
2021-11-03 00:58:40 -0300

    corrigindo request top-five

--- src/services/api.js
+++ src/services/api.js
@@ -16,42 +16,17 @@ export const getApiToken = async (username, password) => {
 export const getProducts = async (token) => {
   
   const headers = {
-    headers: {
-      Authorization: `Baerer ${token}`
-    }
+    Authorization: `Baerer ${token}`
   }
 
-  return api.get('/products', headers)
+  return api.get('/products', { headers: headers })
 };
 
 export const getTopFive = async (token) => {
   
   const headers = {
-      Authorization: `Baerer ${token}`
+    Authorization: `Baerer ${token}`
   }
 
-  return api.post('orders/top-five', { headers })
+  return api.get('orders/top-five', { headers: headers })
 };
-
-const body = [
-  {
-    name: "Pastel de Carne",
-    quantity: 10
-  },
-  {
-    name: "Camarão Cremoso",
-    quantity: 11
-  },
-  {
-    name: "Frango Mineiro",
-    quantity: 12
-  },
-  {
-    name: "Pastel de Queijo",
-    quantity: 13
-  },
-  {
-    name: "Chocolate com Morango",
-    quantity: 14
-  }
-]
