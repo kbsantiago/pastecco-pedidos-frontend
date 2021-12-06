bd4732beb838fd7135667b7393a8b17f1b22d43c
Leonardo Rissi <leonardomelorissi@gmail.com>
2021-12-06 12:12:13 -0300

    fix: tornando login, carwrapper e wrapper responsivos

--- src/components/Card/CardStyles.js
+++ src/components/Card/CardStyles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const CardsWrapper = styled.section`
     max-width: 800px;
-    margin: 0 auto;
+    min-width: 320px;
+    margin: auto;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 10px;
@@ -11,7 +12,7 @@ export const CardsWrapper = styled.section`
 
 export const CardImageContainer = styled.div`
     img {
-        width: 250px;
+        width: 100%;
         height: 150px;
         border-radius: 15px ;
     }
@@ -23,7 +24,9 @@ export const CardContent = styled.div`
 
 export const CardDiv = styled.div``;
 
-export const CardImg = styled.img``;
+export const CardImg = styled.img`
+    width: 100%;
+`;
 
 export const CardTitle = styled.h3`
     font-size: 18px;
--- src/components/CardWrapper/CardWrapperStyles.js
+++ src/components/CardWrapper/CardWrapperStyles.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 
 export const CardsWrapper = styled.section`
-    max-width: 800px;
-    margin: 0 auto;
+    margin: auto;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 10px;
     padding: 14px;
+
+    @media(max-width: 768px){
+        grid-template-columns: auto auto;
+    }
 `;
